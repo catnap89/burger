eb231260685c721fabc46b113dff1aba06cf71c9
youngwoo.cho <petshop0907@gmail.com>
2019-09-10 17:39:23 -0400

    server.js that is the initial starting point for the Node/Express server. express dependency added and it sets up the express and handlebars and starts the server to begin listening.

---
+++ server.js
@@ -0,0 +1,36 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Static directory to be served
+app.use(express.static("public/assets"));
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
