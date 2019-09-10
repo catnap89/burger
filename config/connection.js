a05ef43b50b65873f136a2ecf773d259dcb370e2
youngwoo.cho <petshop0907@gmail.com>
2019-09-10 17:40:43 -0400

    connection.js for setting up the connection to the mysql server

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+var mysql = require("mysql");
+var mysqlPass = require("../keys")
+require("dotenv").config();
+
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: mysqlPass.mysqlConnect,
+  database: "burger_db"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
