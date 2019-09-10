dbd778887cc30bc8e432a15e93bfde56c79f1b00
youngwoo.cho <petshop0907@gmail.com>
2019-09-10 10:10:49 -0400

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+-- Mock Data to start with
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("cheese burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("chicken burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("beef burger", false);
+
+
