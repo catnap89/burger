c83c672c7d1810122e99a1d89ff73beb7f0b310c
youngwoo.cho <petshop0907@gmail.com>
2019-09-13 14:45:09 -0400

    orm.js that have the methods that will execute the necessary MYSQL commands such as selectAll for selecting everything from a table, insertOne for inserting data into a table, etc

--- config/orm.js
+++ config/orm.js
@@ -25,17 +25,17 @@ function objToSql(ob) {
 
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
-        var value = ob[key];
-        // check to skip hidden properties
-        if (Object.hasOwnProperty.call(ob, key)) {
-            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-            if (typeof value === "string" && value.indexOf(" ") >= 0) {
-                value = "'" + value + "'";
-            }
-            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-            // e.g. {sleepy: true} => ["sleepy=true"]
-            arr.push(key + "=" + value);
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+            value = "'" + value + "'";
         }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
     }
 
     // translate array of strings to a single comma-separated string
@@ -46,65 +46,70 @@ function objToSql(ob) {
 // * `insertOne()`
 // * `updateOne()`
 
-// Create the ORM object to perform SQL queries
 var orm = {
-  // Function that returns all table entries
+  // Display all burgers in the db.
   selectAll: function(table, cb) {
-    // Construct the query string that returns all rows from the target table
-    var queryString = "SELECT * FROM " + table;
-    // Perform the database query
+    var queryString = "SELECT * FROM " + table + ";";
+
     connection.query(queryString, function(err, result) {
-      if (err) throw err;
-      // Return results in callback
+      if (err) {
+        throw err;
+      }
       cb(result);
-    })
+    });
   },
-  // Function that insert a single table entry
+  // Add a burger to the db.
   insertOne: function(table, cols, vals, cb) {
-    // Construct the query string that inserts a single row into the target table
-		var queryString = "INSERT INTO " + table;
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
 
-		queryString += " (";
-		queryString += cols.toString();
-		queryString += ") ";
-		queryString += "VALUES (";
-		queryString += printQuestionMarks(vals.length);
-		queryString += ") ";
+    console.log(queryString);
 
-		// console.log(queryString);
-
-		// Perform the database query
-		connection.query(queryString, vals, function(err, result) {
-			if (err) {
-				throw err;
-			}
-
-			// Return results in callback
-			cb(result);
-		});
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err
+      }
+      cb(result);
+    });
   },
-  // Function that updates a single table entry
-	updateOne: function(table, objColVals, condition, cb) {
-		// Construct the query string that updates a single entry in the target table
-		var queryString = "UPDATE " + table;
+  // Set burger devoured status to true.
+  updateOne: function(table, objColVals, condition, cb) { // condition = id
+    var queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
 
-		queryString += " SET ";
-		queryString += objToSql(objColVals);
-		queryString += " WHERE ";
-		queryString += condition;
+    console.log(queryString);
 
-		// console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err
+      }
+      cb(result);
+    });
+  },
+  // Delete a burger from the db.
+  deleteOne: function(table, condition, cb) {  // condition = id
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
 
-		// Perform the database query
-		connection.query(queryString, function(err, result) {
-			if (err) {
-				throw err;
-			}
+    console.log(queryString);
 
-			// Return results in callback
-			cb(result);
-		});
-	}
-}
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err
+      }
+      cb(result);
+    });
+  }
+};
 
+// Export the ORM object in module.exports.
 module.exports = orm;
