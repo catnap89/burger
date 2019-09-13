1562d98274131fe4fc14c67713e21bce39b616bc
youngwoo.cho <petshop0907@gmail.com>
2019-09-13 15:14:23 -0400

    using JAWSDB for connection

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,24 @@ require("dotenv").config();
 var mysql = require("mysql");
 var mysqlPass = require("../keys")
 
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: mysqlPass.mysqlConnect,
-  database: "burger_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: mysqlPass.mysqlConnect,
+    database: "burger_db"
+  })
+}
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: mysqlPass.mysqlConnect,
+//   database: "burger_db"
+// });
 
 connection.connect(function(err) {
   if (err) {
