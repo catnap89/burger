7ab1e5717fe1aeb15f2e1ecbbd88a0655e2daefd
youngwoo.cho <petshop0907@gmail.com>
2019-09-13 14:48:24 -0400

    burger.js call the ORM functions using burger specific input for the ORM

--- models/burger.js
+++ models/burger.js
@@ -1,29 +1,34 @@
 // Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
+// The code that will call the ORM functions using burger specific input for the ORM.
 var burger = {
-    all: function(cb) {
-      orm.selectAll("burgers", "id", function(res) {
-        cb(res);
-      });
-    },
-    create: function(name, cb) {
-      orm.insertOne("burgers", ['burger_name'], name, function(res) {
-        cb(res);
-      });
-    },
-    update: function(objColVals, condition, cb) { // table, objColVals, condition, cb
-      var condition = "id=" + id;
-      orm.update("burgers", objColVals, condition, function(res) {
-          cb(res);
-        });
-    }//,
-    // delete: function(condition, cb) {
-    //     orm.deleteOne("burgers", condition, function(res) {
-    //         cb(res);
-    //     });
-    // }
+  // Display all burgers in the db.
+  selectAll: function(cb) {
+    orm.selectAll("burgers", function(res) { //selectAll from burgers table
+      cb(res);
+    });
+  },
+  // Add a new burger to the db.
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  // Change the devoured status to true.
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {  // condition = id
+      cb(res);
+    });
+  },
+  // Delete a burger from the db.
+  deleteOne: function(condition, cb) {
+    orm.deleteOne("burgers", condition, function(res) { // condition = id
+      cb(res);
+    });
+  }
 };
 
-// Export the database functions for the controller (catsController.js).
+// Export at the end of the burger.js file.
 module.exports = burger;
+
